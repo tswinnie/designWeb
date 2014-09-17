b18423262345c830731c65519d0fe155e936b341
tswinnie <tyrone.swinnie@gmail.com>
2014-09-17 02:23:22 -0400

    made my css and tile work with locals()
    I updated the app.yaml file to show the correct css directory and use
    the locals() to reference the correct css file

--- encapsulated/pages.py
+++ encapsulated/pages.py
@@ -38,6 +38,7 @@ class Page(object):
 #I am going to make the attributes title and css able to be called and edited from my main.py file
     # using getter and setters
 
+
     @property
     def title(self):
         return self.__title  #this is returning the value of title
@@ -53,6 +54,7 @@ class Page(object):
     @css.setter
     def css(self, new_css):
         self.__css = new_css  #this is setting the old value of css to the new value of css
+
 
 
 

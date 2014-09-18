e3c927cfeb88183bdac70a1d16374ca4e7363009
tswinnie <tyrone.swinnie@gmail.com>
2014-09-17 23:53:02 -0400

    Setup working class
    I have got the class working that I am going to use for the 5 different
    people

--- encapsulated/main.py
+++ encapsulated/main.py
@@ -22,9 +22,15 @@ class MainHandler(webapp2.RequestHandler):
 
         # I will create an application that calculates the remaining storage for different users
 
-        #I am going to set up my class that will hold my 5 data objects
-
-        #each user will have a total storage limit of 50GB
+        #now I will hard code the values for each data object
+        m = Michele()
+        m.pictures = 5
+        m.videos = 10
+        m.documents = 1
+        m.music = 3
+        m.apps = 4
+        #write the values to the page
+        self.response.write("this is the result " + str(m.final_storage)) #writing the user_page which has has all the html for my application
 
 
 class Michele(object):
@@ -37,57 +43,19 @@ class Michele(object):
         self.__remaining = 0  #total storage remaining
 
 
-class Jack(object):
-    def __init__(self):
-        self.pictures = 0  #the number of pictures in GB
-        self.videos = 0  #the number of videos in GB
-        self.documents = 0  #the number of documents in GB
-        self.music = 0  #the number of music in GB
-        self.apps = 0  #the number of all files stored in GB
-        self.__remaining = 0  #total storage remaining
 
 
-class Henry(object):
-    def __init__(self):
-        self.pictures = 0  #the number of pictures in GB
-        self.videos = 0  #the number of videos in GB
-        self.documents = 0  #the number of documents in GB
-        self.music = 0  #the number of music in GB
-        self.total = 0  #the number of all files stored in GB
-        self.__remaining = 0  #total storage remaining
-
-
-class Tiffany(object):
-    def __init__(self):
-        self.pictures = 0  #the number of pictures in GB
-        self.videos = 0  #the number of videos in GB
-        self.documents = 0  #the number of documents in GB
-        self.music = 0  #the number of music in GB
-        self.apps = 0  #the number of all files stored in GB
-        self.__remaining = 0  #total storage remaining
-
-
-class Jason(object):
-    def __init__(self):
-        self.pictures = 0  #the number of pictures in GB
-        self.videos = 0  #the number of videos in GB
-        self.documents = 0  #the number of documents in GB
-        self.music = 0  #the number of music in GB
-        self.apps = 0  #the number of all files stored in GB
-        self.__remaining = 0  #total storage remaining
-
-        #now I am going to use a getter to do the calculations for me
-
-        @property
-        def final_storage(self):
+    @property
+    def final_storage(self):
             self.__remaining = 50 - (self.pictures + self.videos + self.documents + self.music + self.apps)  #do the math for the remaining storage
-            return self.final_storage
+            return self.__remaining
 
         #now I will set up my setter
 
-        @final_storage.setter
-        def final_storage(self, new_final_storage):
-            pass # not using my setter right now
+    @final_storage.setter
+    def final_storage(self, new_final_storage):
+            pass  # not using my setter right now
+
 
 
 

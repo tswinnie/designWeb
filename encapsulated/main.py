4c061934481f1aa574cf40f36a8befb65538c89d
tswinnie <tyrone.swinnie@gmail.com>
2014-09-17 23:56:30 -0400

    Created another python page
    I put my code containing my class and my getter and setter in a new
    document named users, so that the code looks better and makes it easier
    to manage my code

--- encapsulated/main.py
+++ encapsulated/main.py
@@ -8,6 +8,8 @@ Encapsulated Calculator
 
 import webapp2
 from pages import Page  #importing the page class
+from users import Storage  #importing the storage class
+
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -32,29 +34,6 @@ class MainHandler(webapp2.RequestHandler):
         #write the values to the page
         self.response.write("this is the result " + str(m.final_storage)) #writing the user_page which has has all the html for my application
 
-
-class Storage(object):
-    def __init__(self):
-        self.pictures = 0  #the number of pictures in GB
-        self.videos = 0  #the number of videos in GB
-        self.documents = 0  #the number of documents in GB
-        self.music = 0  #the number of music in GB
-        self.apps = 0  #the number of all files stored in GB
-        self.__remaining = 0  #total storage remaining
-
-
-
-
-    @property
-    def final_storage(self):
-            self.__remaining = 50 - (self.pictures + self.videos + self.documents + self.music + self.apps)  #do the math for the remaining storage
-            return self.__remaining
-
-        #now I will set up my setter
-
-    @final_storage.setter
-    def final_storage(self, new_final_storage):
-            pass  # not using my setter right now
 
 
 
--- encapsulated/users.py
+++ encapsulated/users.py
@@ -1,4 +1,27 @@
 __author__ = 'tyroneswinnie'
 
 
+class Storage(object):
+    def __init__(self):
+        self.pictures = 0  #the number of pictures in GB
+        self.videos = 0  #the number of videos in GB
+        self.documents = 0  #the number of documents in GB
+        self.music = 0  #the number of music in GB
+        self.apps = 0  #the number of all files stored in GB
+        self.__remaining = 0  #total storage remaining
+
+
+
+
+    @property
+    def final_storage(self):
+            self.__remaining = 50 - (self.pictures + self.videos + self.documents + self.music + self.apps)  #do the math for the remaining storage
+            return self.__remaining
+
+        #now I will set up my setter
+
+    @final_storage.setter
+    def final_storage(self, new_final_storage):
+            pass  # not using my setter right now
+
 

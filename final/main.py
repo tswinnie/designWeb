70ea992bcaf29af7f21a77789ae39f0fafb62e48
tswinnie <tyrone.swinnie@gmail.com>
2014-09-23 00:10:30 -0400

    Set up page class
    I have created a page class that will hold html data

--- final/main.py
+++ final/main.py
@@ -1,24 +1,54 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+'''
+Tyrone Swinnie
+Design for Web
+Final Project: Application with API
+9/22/14
+'''
+
+
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        # self.response.write('Hello world!')
+        p = Page()
+        self.response.write(p.print_out())
+
+# I am going to create my page class
+
+class Page(object):
+    def __init__(self):
+
+        self._head = '''
+
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+
+        <title></title>
+
+    </head>
+    <body> '''
+
+        self._body = ''
+        self._close = '''
+
+    </body>
+
+</html>
+
+        '''
+
+    def print_out(self):
+        return self._head + self._body + self._close
+
+
+
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)

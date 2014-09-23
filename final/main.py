62a4cd602b3b45008b79f4ace4137cceb2eb6d14
tswinnie <tyrone.swinnie@gmail.com>
2014-09-23 00:34:35 -0400

    add my user input class
    I created a class that inheritance the Page class  and alows the user
    to input a value into a form

--- final/main.py
+++ final/main.py
@@ -11,7 +11,8 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         # self.response.write('Hello world!')
-        p = Page()
+        p = UserInput()
+        p.inputs = [['recipe', 'text', 'Search Recipe'], ['Submit', 'submit']]
         self.response.write(p.print_out())
 
 # I am going to create my page class
@@ -32,7 +33,7 @@ class Page(object):
     </head>
     <body> '''
 
-        self._body = ''
+        self._body = 'Test'
         self._close = '''
 
     </body>
@@ -40,9 +41,41 @@ class Page(object):
 </html>
 
         '''
-
+#created function to print out my page
     def print_out(self):
         return self._head + self._body + self._close
+
+
+#I am going to create my form class that will hold the user input value
+
+class UserInput(Page):
+    def __init__(self):
+        #run the constructor function for my Page class
+        super(UserInput, self).__init__()
+        self._form_open = '<form method="GET"'
+        self._form_close = '</form>'
+        self.__inputs = []
+        #<input type="text" value = "" name="recipe" placeholder="Search Recipes" />
+        #<input type="submit" value="Submit" />
+
+        @property
+        def inputs(self):
+            pass
+
+        @inputs.setter
+        def inputs(self, arr):
+            #change my input variables from private
+            self.__inputs = arr
+            #now I am going to sort through my array of inputs
+            print arr
+
+
+
+
+
+
+
+
 
 
 

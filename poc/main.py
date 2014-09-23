2dd469e3a6b5bdfa8b4afc0e4003a29c58262f2c
tswinnie <tyrone.swinnie@gmail.com>
2014-09-22 23:49:33 -0400

    Set up directory
    I set up my directory where I will create my application

--- poc/main.py
+++ poc/main.py
@@ -13,7 +13,7 @@ from xml.dom import minidom   #library used to pars xml
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
-        url = "http://maps.google.com/maps/api/geocode/xml?address=florida&sensor=false" # set up the url to get data, also passing in Florida so that google map returns data on florida
+        url = "http://www.recipepuppy.com/api/?i=onions,garlic&q=pizza&format=xml"   # set up the url to get data, also passing in Florida so that google map returns data on florida
 
         request = urllib2.Request(url)  #set up a variable that wil get the data from the url
 
@@ -23,18 +23,26 @@ class MainHandler(webapp2.RequestHandler):
 
         xmldoc = minidom.parse(result) #I am going to parse the xml data
 
-        new_data = xmldoc.getElementsByTagName("long_name")[0].firstChild.nodeValue  #getting a specific set back from xml
+        print(xmldoc)
 
-        new_data_two = xmldoc.getElementsByTagName("short_name")[0].firstChild.nodeValue  #getting a specific set back from xml
+        new_data = xmldoc.getElementsByTagName("title")[0].firstChild.nodeValue  #getting a specific set back from xml
 
-        latt = xmldoc.getElementsByTagName("lat")[0].firstChild.nodeValue  #testing to see if I can get some data back getting back lattitude
+        new_data_two = xmldoc.getElementsByTagName("href")[0].firstChild.nodeValue  #getting a specific set back from xml
 
-
-        long = xmldoc.getElementsByTagName("lng")[0].firstChild.nodeValue  #testing to see if I can get some data back getting back longitude
-
-        self.response.write("The data that is returned from Google Maps API:  " + " <br/> " + new_data + " <br/> " + new_data_two + " <br/> " + latt + " <br/> " + long)
+        new_data_three = xmldoc.getElementsByTagName("ingredients")[0].firstChild.nodeValue  #getting a specific set back from xml
 
 
+
+        # new_data_two = xmldoc.getElementsByTagName("short_name")[0].firstChild.nodeValue  #getting a specific set back from xml
+
+        # latt = xmldoc.getElementsByTagName("lat")[0].firstChild.nodeValue  #testing to see if I can get some data back getting back lattitude
+
+
+        # long = xmldoc.getElementsByTagName("lng")[0].firstChild.nodeValue  #testing to see if I can get some data back getting back longitude
+
+        # self.response.write("The data that is returned from Google Maps API:  " + " <br/> " + new_data + " <br/> " + new_data_two + " <br/> " + latt + " <br/> " + long)
+
+        # self.response.write(new_data + "<br/>" + new_data_two + "<br/>" + new_data_three)
 
 
 app = webapp2.WSGIApplication([

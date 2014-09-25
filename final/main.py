af79cab9a52ec4f5347c99ee1424b9bec13b504b
tswinnie <tyrone.swinnie@gmail.com>
2014-09-25 01:20:58 -0400

    added mvc development
    I added the mvc development practice to my application and placed all
    my files in the appropriate sections

--- final/main.py
+++ final/main.py
@@ -21,78 +21,169 @@ class MainHandler(webapp2.RequestHandler):
 
         if self.request.GET:
             #get info from api
-            recipe = self.request.GET['recipe']
+            # recipe = self.request.GET['recipe']
+            rd = RecipeModel()
+            rd.recipe = self.request.GET['recipe']
+            rd.callAPI()
+
+            rv = RecipeView() # calling my view class
+            rv.title = "<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 62px; z-index: 999;'>" + "<b>Name of the recipe is:</b>  " + "<br/>" + rd.array_dos[0] + "</div>"  #setting the var I created in my views class equal to the index value of object being returned in array_dos
+            rv.ingredients = "<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 31px; z-index: 999; margin-left: 315px;'>" + "<b>Ingredients:</b> " + rd.array_dos[1] + "</div>" #setting the var I created in my views class equal to the index value of object being returned in array_dos
+            rv.link = "<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 31px; z-index: 999; margin-left: 615px;'>" + "<b>Link to the recipe:</b> " + rd.array_dos[2] + "</div>" #setting the var I created in my views class equal to the index value of object being returned in array_dos
+            rv.source = "<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:106px; z-index: 999; margin-left: 963px;'>" + "<b>Name of the source is:</b> " + rd.array_dos[3] + "</div>" #setting the var I created in my views class equal to the index value of object being returned in array_dos
+            rv.recipe_link = "<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:66px; z-index: 999; margin-left: 963px; '>" + "<b>Link to the source:</b> " + rd.array_dos[4] + "</div>" #setting the var I created in my views class equal to the index value of object being returned in array_dos
+            rv.version_num = "<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:36px; z-index: 999; margin-left: 963px;'>" + "<b>The version number is:</b> " + str(rd.array_dos[5]) + "</div>" #setting the var I created in my views class equal to the index value of object being returned in array_dos
+
+
+            self.response.write(rv.title + rv.ingredients + rv.link + rv.source + rv.recipe_link + str(rv.version_num))
+
+
+
+            #
+            # url = "http://www.recipepuppy.com/api/?i=onions,garlic&q=" + recipe
+            #
+            #
+            # request = urllib2.Request(url)  #set up a variable that wil get the data from the url
+            #
+            # opener = urllib2.build_opener()  #create a var that will represent the object that is returned
+            #
+            # result = opener.open(request)  #using result to get the result from the url and request the data from the API
+            #
 
 
 
 
 
-            url = "http://www.recipepuppy.com/api/?i=onions,garlic&q=" + recipe
-
-
-            request = urllib2.Request(url)  #set up a variable that wil get the data from the url
-
-            opener = urllib2.build_opener()  #create a var that will represent the object that is returned
-
-            result = opener.open(request)  #using result to get the result from the url and request the data from the API
-
-            jsondoc = json.load(result)  #i am parsing my json doc here
-
-            #now I am going to get back specific pieces of data
-
-
-            source = jsondoc['title']
-
-            source_link = jsondoc['href']
-
-            version = jsondoc['version']
-
-
-            img = jsondoc['results'][0]['thumbnail']
-
-            name_list = "<b>The name of this recipe is:</b> " + jsondoc['results'][0]['title']
-
-            ingredients = jsondoc['results'][0]['ingredients']
-
-            link = jsondoc['results'][0]['href']
-
-            self.ingr_list = '' #create var that will hold the value for my items in the array of ingredients
-
-            for item in ingredients: #loop through the items in the array of ingredients
-                self.ingr_list += item  #add each item to the page
-
-
-        #now I am going to assgin the data returned from the json object to my recipe data object
-                self._array_do = []
-                #call my recipe data object method
-                array_object = RecipeData()
-                #now assign the corresponding values returned from json object to objects in recipe object
-                array_object.title = jsondoc['results'][0]['title']
-                array_object.ingredients = jsondoc['results'][0]['ingredients']
-                array_object.link = jsondoc['results'][0]['href']
-                array_object.source = jsondoc['href']
-                array_object.link_source = jsondoc['href']
-                array_object.version_number = jsondoc['version']
-                #now I am going to add these objects to my array
-                self._array_do.append(array_object)
-                print self._array_do
+            #
+            # self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 45px; z-index: 999;'>" +  name_list + "</div>")
+            #
+            # self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 31px; z-index: 999; margin-left: 315px;'>" + "<b>Ingredients:</b> " + self.ingr_list + "</div>")
+            #
+            # self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 31px; z-index: 999; margin-left: 615px;'>" + "<b>Link to the recipe:</b> " + link + "</div>")
+            #
+            # self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:106px; z-index: 999; margin-left: 963px;'>" + "<b>Name of the source is:</b> " + source + "</div>")
+            #
+            # self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:66px; z-index: 999; margin-left: 963px; '>" + "<b>Link to the source:</b> " + source_link + "</div>")
+            #
+            # self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:36px; z-index: 999; margin-left: 963px;'>" + "<b>The version number is:</b> " + str(version) + "</div>")
 
 
 
 
+#now I am going to create my recipe view
+class RecipeView(object):
+    ''' This class controls how the views are displayed to the user'''
+    def __init__(self):
+    #now create an array to hold objects
+        self.__new_array_dos = []
+        self.title = ''
+        self.ingredients = ''
+        self.link = ''
+        self.source = ''
+        self.recipe_link = ''
+        self.version_num = ''
 
 
-            self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 45px; z-index: 999;'>" +  name_list + "</div>")
+    @property
+    def new_array_dos(self):
+        pass
 
-            self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 31px; z-index: 999; margin-left: 315px;'>" + "<b>Ingredients:</b> " + self.ingr_list + "</div>")
+    @new_array_dos.setter
+    def new_array_dos(self, arr):
+        self.__new_array_dos = arr
 
-            self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom: 31px; z-index: 999; margin-left: 615px;'>" + "<b>Link to the recipe:</b> " + link + "</div>")
 
-            self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:106px; z-index: 999; margin-left: 963px;'>" + "<b>Name of the source is:</b> " + source + "</div>")
 
-            self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:66px; z-index: 999; margin-left: 963px; '>" + "<b>Link to the source:</b> " + source_link + "</div>")
+#now I am going to create my model class
+class RecipeModel(object):
+    ''' This class is responsible for fetching, sorting and parsing my data'''
+    def __init__(self):
+        self.__url = "http://www.recipepuppy.com/api/?i=onions,garlic&q="
+        self.__recipe = ''
+        self.__jsondoc = ''
 
-            self.response.write("<div style='margin: 0 auto; width: 195px; display:block; float: left; position: absolute; bottom:36px; z-index: 999; margin-left: 963px;'>" + "<b>The version number is:</b> " + str(version) + "</div>")
+
+#now I am going to make a property that handles the API
+    def callAPI(self):
+        #now I am going to make the request to the api
+
+        request = urllib2.Request(self.__url + self.__recipe)  #set up a variable that wil get the data from the url
+
+        opener = urllib2.build_opener()  #create a var that will represent the object that is returned
+
+        result = opener.open(request)  #using result to get the result from the url and request the data from the API
+
+        self.__jsondoc = json.load(result)  #i am parsing my json doc here
+
+        #now I am going to get back specific pieces of data
+        #I am going to parse the data here
+
+
+        source = self.__jsondoc['title']
+
+        source_link = self.__jsondoc['href']
+
+        version = self.__jsondoc['version']
+
+        img = self.__jsondoc['results'][0]['thumbnail']
+
+        name_list = "<b>The name of this recipe is:</b> " + self.__jsondoc['results'][0]['title']
+
+        ingredients = self.__jsondoc['results'][0]['ingredients']
+
+        link = self.__jsondoc['results'][0]['href']
+
+        self.ingr_list = '' #create var that will hold the value for my items in the array of ingredients
+
+        for item in ingredients: #loop through the items in the array of ingredients
+            self.ingr_list += item  #add each item to the page
+
+
+    #now I am going to assgin the data returned from the json object to my recipe data object
+            self._array_do = []
+            #call my recipe data object method
+            array_object = RecipeData()
+            #now assign the corresponding values returned from json object to objects in recipe object
+            array_object.title = self.__jsondoc['results'][0]['title']
+            array_object.ingredients = self.__jsondoc['results'][0]['ingredients']
+            array_object.link = self.__jsondoc['results'][0]['href']
+            array_object.source = self.__jsondoc['href']
+            array_object.link_source = self.__jsondoc['href']
+            array_object.version_number = self.__jsondoc['version']
+            #now I am going to add these objects to my array
+            # self._array_do.append(array_object)
+            self._array_do.append(array_object.title)
+            self._array_do.append(array_object.ingredients)
+            self._array_do.append(array_object.link)
+            self._array_do.append(array_object.source)
+            self._array_do.append(array_object.link_source)
+            self._array_do.append(array_object.version_number)
+
+
+
+
+
+
+
+
+    @property
+    def array_dos(self):
+        return self._array_do
+
+
+
+
+    @property
+    def recipe(self):
+        pass
+
+    @recipe.setter
+    def recipe(self, r):
+        self.__recipe = r
+
+
+
+
 
 
 
@@ -211,7 +302,7 @@ class UserInput(Page):
 
 #now I am going to create a function that will print out my form
     def show_form(self):
-        new_form = self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
+        new_form = self._head + self._form_open + self._form_inputs + self._form_close + self._body + self._close
 
         new_form = new_form.format(**locals())
         return new_form
